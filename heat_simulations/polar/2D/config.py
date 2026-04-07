cfa2c6c5d4b5d59275a159ada8742a9e84a0abff
jackbernsteinsheehan <jackbern@uoregon.edu>
2026-04-06 19:47:58 -0700

    updated config

--- heat_simulations/polar/2D/config.py
+++ heat_simulations/polar/2D/config.py
@@ -5,14 +5,16 @@ Nr = 25
 Ntheta = 25
 
 # can be True, False, "single"
-CURVE_PLOT = "single" # Show curvature flow plot, or show a single frame with "single"
+CURVE_PLOT = False # Show curvature flow plot, or show a single frame with "single"
 SAVE_EVERY = 200 # How often we save the state to a new frame
 BETA = 0
 RHO = 0
+
+
 
 # --- Geodesic params --- #
 P = 0.75 + 0.0j
 V = -0.5 + 0.35j
 U_IDX = 50 # determines which weight function from the frames object will be used
-STEPS = 5000
+STEPS = 5000 # Number of iterations in geodesic calculation
 GEO_PLOT = True # Show geodesic path on a 2D circle
